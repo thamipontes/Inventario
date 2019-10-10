be72e79905a4d72fc3d58bd9626b9c500878cfa3
thamipontes <thamiresdepontesdasilva@gmail.com>
2019-10-10 14:04:54 -0300

    Projeto Spring Boot

---
+++ Projeto Web/inventario/src/test/java/com/prf/inventario/InventarioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prf.inventario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InventarioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
